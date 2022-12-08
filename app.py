4863cf967a1d6748ee7b7e07b40452327e8080f0
Parul Bhardwaj <bhardwaj.parul88@gmail.com>
2022-12-08 13:30:40 -0500

    Comments

--- app.py
+++ app.py
@@ -15,12 +15,10 @@ mongo = PyMongo(app)
 # ROUTES
 ############################################################
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> Home Page >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/')
 def plants_list():
     """Display the plants list page."""
-
-    # TODO: Replace the following line with a database call to retrieve *all*
-    # plants from the Mongo database's `plants` collection.
     plants_data = mongo.db.plants.find()
 
     context = {
@@ -28,17 +26,19 @@ def plants_list():
     }
     return render_template('plants_list.html', **context)
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> about Page >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/about')
 def about():
     """Display the about page."""
     return render_template('about.html')
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> New plant : Create >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     """Display the plant creation page & process data from the creation form."""
     if request.method == 'POST':
-        # TODO: Get the new plant's name, variety, photo, & date planted, and 
-        # store them in the object below.
+        # Got the new plant's name, variety, photo, & date planted, and 
+        # stored them in the object below.
         name = request.form['plant_name']
         variety = request.form['variety']
         photo = request.form['photo']
@@ -49,28 +49,18 @@ def create():
             'photo': photo,
             'date_planted': date_planted
         }
-        # TODO: Make an `insert_one` database call to insert the object into the
-        # database's `plants` collection, and get its inserted id. Pass the 
-        # inserted id into the redirect call below.
         new_plant_id = mongo.db.plants.insert_one(new_plant).inserted_id
         return redirect(url_for('detail', plant_id=new_plant_id))
 
     else:
         return render_template('create.html')
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> Plant Details >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/plant/<plant_id>')
 def detail(plant_id):
     """Display the plant detail page & process data from the harvest form."""
 
-    # TODO: Replace the following line with a database call to retrieve *one*
-    # plant from the database, whose id matches the id passed in via the URL.
     plant_to_show = mongo.db.plants.find_one({"_id": ObjectId(plant_id)})
-    # plant_to_show = mongo.db.plants.find_one(plantID=plant_id)
-
-    # TODO: Use the `find` database operation to find all harvests for the
-    # plant's id.
-    # HINT: This query should be on the `harvests` collection, not the `plants`
-    # collection.
     harvests = mongo.db.harvests.find()
 
     context = {
@@ -79,14 +69,13 @@ def detail(plant_id):
     }
     return render_template('detail.html', **context)
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> Harvest >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/harvest/<plant_id>', methods=['POST'])
 def harvest(plant_id):
     """
     Accepts a POST request with data for 1 harvest and inserts into database.
     """
 
-    # TODO: Create a new harvest object by passing in the form data from the
-    # detail page form.
     quantity = request.form['harvested_amount']
     date = request.form['date_planted']
     new_harvest = {
@@ -95,18 +84,16 @@ def harvest(plant_id):
         'plant_id': plant_id
     }
 
-    # TODO: Make an `insert_one` database call to insert the object into the 
-    # `harvests` collection of the database.
     mongo.db.harvests.insert_one(new_harvest)
-    # return redirect(url_for('detail', plant_id=new_harvest['_id']))
     return redirect(url_for('detail', plant_id=plant_id))
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> Edit a Plant >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/edit/<plant_id>', methods=['GET', 'POST'])
 def edit(plant_id):
     """Shows the edit page and accepts a POST request with edited data."""
     if request.method == 'POST':
-        # TODO: Make an `update_one` database call to update the plant with the
-        # given id. Make sure to put the updated fields in the `$set` object.
+        # Made an `update_one` database call to update the plant with the
+        # given id. Put the updated fields in the `$set` object.
         name = request.form['plant_name']
         variety = request.form['variety']
         photo = request.form['photo']
@@ -131,6 +118,7 @@ def edit(plant_id):
 
         return render_template('edit.html', **context)
 
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>> Delete >>>>>>>>>>>>>>>>>>>>>>>>>>>>
 @app.route('/delete/<plant_id>', methods=['POST'])
 def delete(plant_id):
 
